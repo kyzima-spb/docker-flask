4b759a8d1e2180c171c0d74a65158a63fab21cca
Kirill Vercetti <office@kyzima-spb.com>
2020-08-03 16:28:09 +0300

    Добавлена возможность запускать команды flask и gunicorn от обычного пользователя

--- Dockerfile
+++ Dockerfile
@@ -4,16 +4,22 @@ LABEL maintainer="Kirill Vercetti <office@kyzima-spb.com>"
 
 ENV DEBIAN_FRONTEND noninteractive
 ENV PYTHONPATH "/python_packages"
+ENV USER_UID 1000
+ENV USER_GID 1000
 ENV FLASK_ENV production
 ENV WORKER_CLASS gevent
 ENV TIMEOUT 3000
 
 EXPOSE 5000
 
+RUN set -x \
+    && groupadd -g 1000 user \
+    && useradd -u 1000 -g user -s /bin/bash -m user
+
 RUN apt update
 
 RUN set -x \
-    && apt install -yq --no-install-recommends gettext-base \
+    && apt install -yq --no-install-recommends gettext-base gosu \
     && apt-get clean  \
     && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
@@ -24,4 +30,4 @@ RUN ln -s /usr/local/bin/docker-entrypoint.sh /entrypoint.sh \
     && chmod +x /usr/local/bin/pydep.sh
 
 ENTRYPOINT ["docker-entrypoint.sh"]
-CMD ["run"]
+CMD ["flask", "run"]
--- root/usr/local/bin/docker-entrypoint.sh
+++ root/usr/local/bin/docker-entrypoint.sh
@@ -1,21 +1,41 @@
 #!/usr/bin/env bash
 set -e
 
+if [[ -z $USER_UID ]]; then
+    USER_UID=$(id -u)
+fi
 
-if [[ "$1" = 'run' ]]; then
-    case "$FLASK_ENV" in
-        'development')
-            flask run --host=0.0.0.0
-            ;;
-        'production')
-            envsubst < /gunicorn_config.tmpl > /gunicorn_config.py
-            gunicorn -c /gunicorn_config.py $FLASK_APP
-            ;;
-        *)
-            echo "Unknow environment $FLASK_ENV" >&2
-            exit 1
-            ;;
-    esac
+if [[ -z $USER_GID ]]; then
+    USER_GID=$(id -g)
+fi
+
+if [[ "$1" = 'flask' ]]; then
+    echo "Run Flask CLI; User: $USER_UID, Command: $@"
+
+    if [[ "$USER_UID" = '0' ]]; then
+        chown -R $USER_UID:$USER_GID /home/user
+        usermod -u $USER_UID user
+        groupmod -g $USER_GID user
+
+		# then restart script as user
+		exec gosu user "$BASH_SOURCE" "$@"
+	fi
+
+    if [[ "$2" = 'run' ]]; then
+        case "$FLASK_ENV" in
+            'development')
+                flask run --host=0.0.0.0
+                ;;
+            'production')
+                envsubst < /gunicorn_config.tmpl > $HOME/gunicorn_config.py
+                gunicorn -c $HOME/gunicorn_config.py $FLASK_APP
+                ;;
+            *)
+                echo "Unknow environment $FLASK_ENV" >&2
+                exit 1
+                ;;
+        esac
+    fi
 fi
 
 exec "$@"
