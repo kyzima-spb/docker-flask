5e7757e22f67d89405170488adceb3e62fbb6462
Kirill Vercetti <office@kyzima-spb.com>
2021-11-20 19:20:21 +0300

    Demo example with python info

---
+++ root/app/app.py
@@ -0,0 +1,28 @@
+import os
+import platform
+
+from flask import Flask, render_template
+
+
+def pythoninfo():
+    return {
+        'version': platform.python_version(),
+        'system': ' '.join(platform.uname()),
+        'build': {
+            'implementation': platform.python_implementation(),
+            'build_date': platform.python_build()[1],
+            'compiler': platform.python_compiler(),
+            'scm_branch': platform.python_branch(),
+            'scm_revision': platform.python_revision(),
+        },
+        'platform': platform,
+        'env': os.environ,
+    }
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html', **pythoninfo())
