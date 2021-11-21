687d016268e7fe24aece9f20775a61c98b875c29
Kirill Vercetti <office@kyzima-spb.com>
2021-11-21 22:13:59 +0300

    Refactoring directories

--- update.sh
+++ update.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-versions=( "$@" )
+cmd="${@:1:1}"
+versions=( "${@:2}" )
+
+
 if [ ${#versions[@]} -eq 0 ]; then
     versions=( [0-9].[0-9]/ )
 fi
@@ -19,25 +22,38 @@ EOH
 }
 
 for version in "${versions[@]}"; do
-    for base in alpine{3.12,3.13} {stretch,buster}{/slim,}; do
+    for base in alpine{3.12,3.13,3.14} {slim-,}{stretch,buster,bullseye}; do
         dest="$version/$base"
         distr="$(basename "$base")"
         
         case "$base" in
-            *slim) template="slim"; tag="slim-$(dirname "$base")" ;;
-            alpine*) template="alpine"; tag="${base}" ;;
+            slim*) template="slim"; tag="$base" ;;
+            alpine*) template="alpine"; tag="$base" ;;
             *) template="debian"; tag="$distr" ;;
         esac
         
-        echo "$base => FROM python:$version-$tag"
-        
         [ -d "$dest" ] || continue
         
-        template="Dockerfile-${template}.template"
-        { generated_warning; cat "$template"; } > "$dest/Dockerfile"
-
-        sed -ri \
-            -e 's/^(FROM python):%%PLACEHOLDER%%/\1:'"$version-$tag"'/' \
-    		"$dest/Dockerfile"
+        case "$cmd" in
+            generate)
+                echo "$base => FROM python:$version-$tag => $dest"
+        
+                template="Dockerfile-${template}.template"
+                { generated_warning; cat "$template"; } > "$dest/Dockerfile"
+        
+                sed -ri \
+                    -e 's/^(FROM python):%%PLACEHOLDER%%/\1:'"$version-$tag"'/' \
+                "$dest/Dockerfile"
+                ;;
+            build)
+                docker buildx build \
+                    --platform linux/386,linux/amd64,linux/arm/v7,linux/arm64 \
+                    -t "kyzimaspb/flask:$version-$tag" \
+                    -f "$dest/Dockerfile" .
+                ;;
+            *)
+              echo "Unknown command"
+              ;;
+        esac
     done
 done
