3c72b79fd8d91ea94108e4c329b5815652bbac53
Kirill Vercetti <office@kyzima-spb.com>
2023-01-16 19:17:40 +0300

    Added run Flask by root user

--- docker/root/usr/local/bin/docker-entrypoint.sh
+++ docker/root/usr/local/bin/docker-entrypoint.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
-set -e
+# set -e
 
 commandExists()
 {
     command -v "$1" > /dev/null 2>&1
+}
+
+
+userExists()
+{
+    getent passwd $1 > /dev/null 2>&1
+}
+
+
+groupExists()
+{
+    getent group $1 > /dev/null 2>&1
 }
 
 
@@ -16,20 +28,26 @@ if [[ -z $USER_GID ]]; then
 fi
 
 if [[ "$1" = 'flask' ]]; then
-    echo "Run Flask CLI; User: $(id -u), Command: $@"
+    echo "Run Flask CLI; User: $(id -u), Group: $(id -g), Command: $@"
 
-    if [[ "$(id -u)" = '0' ]]; then
-        chown -R $USER_UID:$USER_GID /home/user /app
-        usermod -u $USER_UID user
-        groupmod -g $USER_GID user
-
-		# then restart script as user
-        if commandExists "gosu"; then
-            exec gosu user "$BASH_SOURCE" "$@"
-        else
-            exec su-exec user "$BASH_SOURCE" "$@"
+    if [[ "$(id -u)" = '0' ]] && [[ "$USER_UID" != '0' ]]; then
+        if ! userExists "$USER_UID"; then
+            usermod -u "$USER_UID" user
         fi
-	fi
+        
+        if ! groupExists "$USER_GID"; then
+            groupmod -g "$USER_GID" user
+        fi
+
+        chown -R $USER_UID:$USER_GID /home/user /app
+
+		    # then restart script as user
+        if commandExists "gosu"; then
+            exec gosu $USER_UID:$USER_GID "$BASH_SOURCE" "$@"
+        else
+            exec su-exec $USER_UID:$USER_GID "$BASH_SOURCE" "$@"
+        fi
+    fi
 
     if [[ "$2" = 'run' ]]; then
         case "$FLASK_ENV" in
