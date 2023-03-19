1cecd4257404f42e0d8270ce2375867d7a3b1ae3
Kirill Vercetti <office@kyzima-spb.com>
2023-03-19 13:25:09 +0300

    Внешний скрипт для переключения пользователя

--- docker/root/usr/local/bin/docker-entrypoint.sh
+++ docker/root/usr/local/bin/docker-entrypoint.sh
@@ -1,22 +1,5 @@
 #!/usr/bin/env bash
 
-commandExists()
-{
-    command -v "$1" > /dev/null 2>&1
-}
-
-
-userExists()
-{
-    getent passwd $1 > /dev/null 2>&1
-}
-
-
-groupExists()
-{
-    getent group $1 > /dev/null 2>&1
-}
-
 
 getPythonPackageVersion()
 {
@@ -24,69 +7,57 @@ getPythonPackageVersion()
 }
 
 
-export PYTHON_FLASK_VERSION="$(getPythonPackageVersion "Flask")"
+export PYTHON_FLASK_VERSION
+PYTHON_FLASK_VERSION="$(getPythonPackageVersion "Flask")"
 export GUNICORN_CONFIG_LOCATION="/etc/gunicorn"
 
 if [[ -z $USER_UID ]]; then
-    USER_UID=$(id -u)
+  USER_UID=$(id -u)
 fi
 
 if [[ -z $USER_GID ]]; then
-    USER_GID=$(id -g)
+  USER_GID=$(id -g)
+fi
+
+if [[ "$(id -u)" = '0' ]] && [[ "$USER_UID" != '0' ]]; then
+  exec switch-user.sh -v \
+    -d /app \
+    -d "$GUNICORN_CONFIG_LOCATION" \
+    -e "$BASH_SOURCE" \
+      "$USER_UID:$USER_GID" "$*"
 fi
 
 if [[ "$1" = 'flask' ]]; then
-    echo "Run Flask CLI; User: $(id -u), Group: $(id -g), Command: $@"
-
-    if [[ "$(id -u)" = '0' ]] && [[ "$USER_UID" != '0' ]]; then
-        if ! userExists "$USER_UID"; then
-            usermod -u "$USER_UID" user
-        fi
-        
-        if ! groupExists "$USER_GID"; then
-            groupmod -g "$USER_GID" user
-        fi
-
-        chown -R $USER_UID:$USER_GID /home/user "$GUNICORN_CONFIG_LOCATION" /app
-
-		    # then restart script as user
-        if commandExists "gosu"; then
-            exec gosu $USER_UID:$USER_GID "$BASH_SOURCE" "$@"
-        else
-            exec su-exec $USER_UID:$USER_GID "$BASH_SOURCE" "$@"
-        fi
-    fi
-    
-    if [[ "$PYTHON_FLASK_VERSION" < '1.0.0' ]]; then
-        echo "Unsupported Flask version $FLASK_VERSION" >&2
+  if [[ "$PYTHON_FLASK_VERSION" < '1.0.0' ]]; then
+    echo "Unsupported Flask version $FLASK_VERSION" >&2
+    exit 1
+  elif [[ "$PYTHON_FLASK_VERSION" < '2.2.0' ]]; then
+    FLASK_ENV=${FLASK_ENV:-'production'}
+    case "$FLASK_ENV" in
+      'development') IS_DEBUG=true ;;
+      'production') IS_DEBUG=false ;;
+      *)
+        echo "Unknown environment $FLASK_ENV" >&2
         exit 1
-    elif [[ "$PYTHON_FLASK_VERSION" < '2.2.0' ]]; then
-        FLASK_ENV=${FLASK_ENV:-'production'}
-        case "$FLASK_ENV" in
-            'development') IS_DEBUG=true ;;
-            'production') IS_DEBUG=false ;;
-            *)
-                echo "Unknow environment $FLASK_ENV" >&2
-                exit 1
-                ;;
-        esac
+        ;;
+    esac
+  else
+    if [[ "${FLASK_DEBUG:-0}" = '0' ]]; then
+      IS_DEBUG=false
     else
-        FLASK_DEBUG=${FLASK_DEBUG:-'0'}
-        case "$FLASK_DEBUG" in
-            '0') IS_DEBUG=false ;;
-            *) IS_DEBUG=true ;;
-        esac
+      IS_DEBUG=true
     fi
+  fi
 
-    if [[ "$2" = 'run' ]]; then
-        if $IS_DEBUG; then
-            exec flask run --host=0.0.0.0
-        else
-            export GUNICORN_CONFIG_FILE="$GUNICORN_CONFIG_LOCATION/config.py"
-            envsubst < "$GUNICORN_CONFIG_LOCATION/config.tmpl" > "$GUNICORN_CONFIG_FILE"
-            exec gunicorn -c "$GUNICORN_CONFIG_FILE" "$FLASK_APP"
-        fi
+  if [[ "$2" = 'run' ]]; then
+    if $IS_DEBUG; then
+      exec flask run --host=0.0.0.0
+    else
+      export GUNICORN_CONFIG_FILE="$GUNICORN_CONFIG_LOCATION/config.py"
+      envsubst < "$GUNICORN_CONFIG_LOCATION/config.tmpl" > "$GUNICORN_CONFIG_FILE"
+      exec gunicorn -c "$GUNICORN_CONFIG_FILE" "$FLASK_APP"
     fi
+  fi
 fi
 
 exec "$@"
